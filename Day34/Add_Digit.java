1fd45aa73a5e0e5ee93746b735a79505c461855b
Uma Yerjal <yerjaluma2003@gmail.com>
2023-10-26 11:51:48 +0530

    Added the Day34 Program

---
+++ Day34/Add_Digit.java
@@ -0,0 +1,24 @@
+package Day34;
+/*
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it.
+ */
+public class Add_Digit {
+    public static void main(String[] args) {
+        int num = 38;
+        System.out.println(addDigits(num));
+    }
+    public static int addDigits(int num) {
+        if(num==0) return 0;
+        if(num%9==0) return 9;
+        return num%9;
+    }
+}
