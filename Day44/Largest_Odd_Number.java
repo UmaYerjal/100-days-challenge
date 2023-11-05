49a66756dda783166ad27aa92c1c3ad1c5d47379
Uma Yerjal <yerjaluma2003@gmail.com>
2023-11-05 23:53:04 +0530

    added the day44 Program

---
+++ Day44/Largest_Odd_Number.java
@@ -0,0 +1,41 @@
+package Day44;
+/*
+ou are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
+
+A substring is a contiguous sequence of characters within a string.
+
+Example 1:
+
+Input: num = "52"
+Output: "5"
+Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the only odd number.
+Example 2:
+
+Input: num = "4206"
+Output: ""
+Explanation: There are no odd numbers in "4206".
+Example 3:
+
+Input: num = "35427"
+Output: "35427"
+Explanation: "35427" is already an odd number.
+ 
+ */
+public class Largest_Odd_Number {
+    public static void main(String[] args) {
+        String str ="35427";
+        System.out.println(largestOddNumber(str));
+
+    }
+    public static String largestOddNumber(String num) {
+        int n = num.length();
+
+        for(int i=n-1; i>=0; i--){
+            int a = num.charAt(i)-48;
+            if(a%2!=0){
+                return num.substring(0,i+1);
+            }
+        }
+        return "";
+    }
+}
