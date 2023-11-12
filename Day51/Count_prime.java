5dd479691e46069a62738e04bde4db8cb3a6d920
Uma Yerjal <yerjaluma2003@gmail.com>
2023-11-12 22:24:45 +0530

    Added the new Program

---
+++ Day51/Count_prime.java
@@ -0,0 +1,36 @@
+package Day51;
+/*
+Given an integer n, return the number of prime numbers that are strictly less than n.
+
+ 
+
+Example 1:
+
+Input: n = 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+Example 2:
+
+Input: n = 0
+Output: 0
+Example 3:
+
+Input: n = 1
+Output: 0
+ */
+public class Count_prime {
+    public static void main(String[] args) {
+        int n = 10;;
+        boolean[] notPrime = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (notPrime[i] == false) {
+                count++;
+                for (int j = 2; i * j < n; j++) {
+                    notPrime[i * j] = true;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
