38659bf5ac5e401e0dc922855ec141aaaae3276b
Uma Yerjal <yerjaluma2003@gmail.com>
2023-12-19 16:28:00 +0530

    Added the Day87 Program

---
+++ Day87/Average_Value_Of_Even_Number_Thet_are_Divisible_By_Three.java
@@ -0,0 +1,37 @@
+package Day87;
+/*
+Given an integer array nums of positive integers, return the average value of all even integers that are divisible by 3.
+
+Note that the average of n elements is the sum of the n elements divided by n and rounded down to the nearest integer.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,6,10,12,15]
+Output: 9
+Explanation: 6 and 12 are even numbers that are divisible by 3. (6 + 12) / 2 = 9.
+Example 2:
+
+Input: nums = [1,2,4,7,10]
+Output: 0
+Explanation: There is no single number that satisfies the requirement, so return 0.
+ */
+public class Average_Value_Of_Even_Number_Thet_are_Divisible_By_Three {
+    public static void main(String[] args) {
+        int arr[] ={4,4,9,10};
+        System.out.println(averageValue(arr));
+    }
+    public static int averageValue(int[] nums) {
+        int k=0;
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%6==0){
+                sum+=nums[i];
+                k++;
+            }
+        }
+        return k>0?sum/k:0;
+    }
+    
+}
